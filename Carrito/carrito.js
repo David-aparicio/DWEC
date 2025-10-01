f0d37c9916df4bb25c6ef1c9f1e3f779c182eba2
COMPU-S034\daw2 <neiser.pintos21@gmail.com>
2025-10-01 16:47:13 +0200

    1/10/2025

--- Carrito/carrito.js
+++ Carrito/carrito.js
@@ -12,7 +12,7 @@ data.products.forEach((producto) => {
 */
 
 class Producto {
-  constructor(sku, title, price){
+  constructor(sku, title, price) {
     this.sku = sku;
     this.title = title;
     this.price = parseFloat(price);
@@ -21,13 +21,21 @@ class Producto {
 class Carrito {
   constructor(currency) { /*Pongo en el constructor solo la divisa ya que el carrito se crea sin productos inicialmente, luego añadire ya un metodo para añadir esos productos al carrito */
     this.currency = currency;
-    this.products = [] /*array de productos vacio pq inicialmente no hay nada*/
+    this.products = []; /*array de productos vacio pq inicialmente no hay nada*/
   }
 
-añadirProducto(nuevoProducto) {
-  this.products.push(nuevoProducto);
-}
-/*Me faltaria un metodo getTotal que recorra sume y me de en formato  */
+  añadirProducto(nuevoProducto) {
+    this.products.push(nuevoProducto);
+  }
+  /*
+  De momento no voy a usar este metodo 
+  getTotal() {
+    return this.products.reduce(function (p, p2) {
+      return p + p2;
+    })
+  }
+    
+  */
 }
 
 export { Carrito, Producto };
--- Carrito/main.js
+++ Carrito/main.js
@@ -8,12 +8,27 @@ const carrito = new Carrito(data.currency); /*Creo el constructor carrito y lo r
 productos o mostrarlos  */
 
 
-/* ns si esta bien pero algo asi seria con map -- 
+/* ns si esta bien pero algo asi seria con map -- */
 carrito.products = data.products.map(function(p) {
 return new Product(p.SKU, p.title, p.price);
-});*/
-
-data.products.forEach(function(p) { /**Uso el forEachh temporalmente pq me he dado cuenta que lo tengo que hacer con map pq luego pa sumar va a ser una liada */
-  carrito.añadirProducto(new Producto(p.SKU, p.title, p.price)); /**Preguntar a anuar pq el price me lo devuelve como un String */
 });
+
+//data.products.forEach(function(p) {                                                      /**Uso el forEachh temporalmente pq me he dado cuenta que lo tengo que hacer con map pq luego pa sumar va a ser una liada */
+  //carriton.añadirProducto(new Producto(p.SKU, p.title, p.price));                        /**Preguntar a anuar pq el price me lo devuelve como un String */
+//});
+
+
+
+
 console.log(carrito);
+
+
+
+
+    document.addEventListener('DOMContentLoaded', function () {
+
+
+
+
+
+      });
