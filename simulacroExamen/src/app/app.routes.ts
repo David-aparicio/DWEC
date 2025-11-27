bb586fe5995af76f2bb2ce59c2c4d834137ee23c
COMPU-S034\daw2 <neiser.pintos21@gmail.com>
2025-11-27 18:32:55 +0100

    clase

--- simulacroExamen/src/app/app.routes.ts
+++ simulacroExamen/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home-component/home-component';
+import { FormularioComponent } from './pages/formulario-component/formulario-component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home'},
     { path: 'home', component: HomeComponent},
+    { path: 'formulario', component: FormularioComponent}
 ];
