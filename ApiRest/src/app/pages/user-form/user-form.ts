ba4a72c4289a065b75d61872d68bd95eaba1892e
COMPU-S034\daw2 <neiser.pintos21@gmail.com>
2025-12-18 19:03:59 +0100

    apirest clase

--- ApiRest/src/app/pages/user-form/user-form.ts
+++ ApiRest/src/app/pages/user-form/user-form.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink, ActivatedRoute, Router } from '@angular/router';
+import { UsuarioService } from '../../services/usuario-service';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './user-form.html',
   styleUrl: './user-form.css',
 })
 export class UserForm {
+  // userForm: FormGroup;
+  // Suser = inject(UsuarioService);
+  // ActivatedRoute = inject(ActivatedRoute);
+  // router = inject(Router);
+  // isNew: boolean;
+  // const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+  // constructor() {
+  //   this.isNew = true;
+  //   this.userForm = this.createForm();
+  // }
+
+  // // private createForm(): FormGroup {
+  // //   return new FormGroup({
+  // //     _id: new FormControl(null),
+  // //     id: new FormControl(null),
+  // //     first_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  // //     last_name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  // //     username: new FormControl('', [Validators.required]),
+  // //     email: 
+      
+  // //   });
+  // // }
 }
--- ApiRest/src/app/services/usuario-service.ts
+++ ApiRest/src/app/services/usuario-service.ts
@@ -1,8 +1,9 @@
+import { Iapi } from './../interfaces/iapi';
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Iusuario } from '../interfaces/iusuario';
 import { lastValueFrom } from 'rxjs';
-import { Iapi } from '../interfaces/iapi';
+
 
 @Injectable({
   providedIn: 'root',
@@ -12,17 +13,15 @@ export class UsuarioService {
     private baseURL: string = 'https://peticiones.online/api/users';
     httpClient = inject(HttpClient);
 
-   
-   
-   
-
 
     constructor(){}
 
 
-    async getAllUsers(): Promise<Iusuario[]> {        // Método asíncrono que me devuelve una promesa de un array de Iusuario
-      const resp = await lastValueFrom(this.httpClient.get<Iapi>(this.baseURL)); // Realiza la petición HTTP GET y espera la respuesta tipada como Iapi
-      return resp.results; // Devuelve el array de usuarios que se llama results
+    async getAllUsers(page: number = 1): Promise<Iapi> {  
+      
+      const resp = await lastValueFrom(this.httpClient.get<Iapi>(`${this.baseURL}?page= ${page}`)); 
+      // Realiza la petición HTTP GET y espera la respuesta tipada como Iapi
+      return resp;// Ahora devolvemos todo el objeto Iapi, no solo results
     }
 
 
